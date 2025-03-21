b3f9d2c5db654c8c490242c8f38d41a5b6464adc
21arkumar <21arkumar@gmail.com>
2025-03-21 13:41:53 +0530

    A1542 CodeForces

---
+++ CodeForces/A1542.java
@@ -0,0 +1,35 @@
+package CodeForces;
+
+import java.util.*;
+public class A1542{
+    public static void solve(int[] arr, int size){
+        int even = 0;
+        int odd = 0;
+        
+        for(int i = 0; i<size; i++){
+            if(arr[i]%2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        if(even == odd){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int arr[] = new int[2*n];
+            for(int i = 0; i<2*n; i++){
+                arr[i] = sc.nextInt();
+            }
+            solve(arr, 2*n);
+        }
+        sc.close();
+    }
+}
